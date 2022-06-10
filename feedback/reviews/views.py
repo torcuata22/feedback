cca0527a7af26dd1adb64e8abc7f27ed311038ed
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-10 16:25:41 -0400

    .is_valid method

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -5,10 +5,12 @@ from .forms import ReviewForm
 # Create your views here.
 
 def review(request):
-    # if request.method == 'POST':
-    #     entered_username = request.POST["username"] #extracts incoming data
-    #     print(entered_username)                     #prints incoming data
-    #     return HttpResponseRedirect("/thank_you")   # redirects to new page, creating GET request instead of another from method=POST
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+         
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank_you")   # redirects to new page, creating GET request instead of another from method=POST
         
 
     form = ReviewForm()        
