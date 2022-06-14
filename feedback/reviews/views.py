7df1a7bd52b6e737399f5b6d47374b7330627891
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-14 16:37:51 -0400

    populate model

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm 
+from .models import Review
 
 # Create your views here.
 
@@ -9,6 +10,7 @@ def review(request):
         form = ReviewForm(request.POST)
          
         if form.is_valid():
+            review = Review(user_name = form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating']) 
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank_you")   # redirects to new page, creating GET request instead of another from method=POST
     
