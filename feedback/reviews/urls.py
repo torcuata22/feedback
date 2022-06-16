4495f0b3d55fdf0f56d96cf55ddfda6499f4a2f4
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-16 15:46:19 -0400

    fixed template folder

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+#from feedback.reviews.views import ReviewsListViews
+#from reviews import views
 
 url_patterns=[
-    path("",views.review)
+ 
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -51,13 +51,14 @@ class ThankYouView(TemplateView):
         context['message'] = "This works!"
         return context
 
-class ReviewListView(TemplateView):
+
+class ReviewsListView(TemplateView):
     template_name= "reviews/review_list.html"
     
     def get_context_data(self, **kwargs):
         context= super().get_context_data(**kwargs)
         reviews=Review.objects.all()
-        context['reviews']=reviews
+        context["reviews"]= reviews
         return context
         
     
