eebb49578d12c485165b379c47ec9e462f71c1de
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-22 11:28:21 -0400

    create profiles app

---
+++ feedback/profiles/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, "profiles/create_profile.html")
+
+    def post(self, request):
+        pass
