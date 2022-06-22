c7cd819eb6d07cbff8b8b92c5a48de5cfe10a6d0
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-22 12:51:44 -0400

    file uploads

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -9,4 +10,7 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        request.FILES["image"]
+        return HttpResponseRedirect("/profiles")
+    
+        
