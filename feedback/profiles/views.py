aff99fa890fc16c7965be66ecfb89635f120ba13
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-22 14:31:47 -0400

    form with FileField

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from .forms import ProfileForm
 
 # Create your views here.
-
-
+#helper finction:
+def store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+        
+    
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        request.FILES["image"]
+        store_file(request.FILES["image"])
         return HttpResponseRedirect("/profiles")
     
         
