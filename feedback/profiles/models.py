fc3b4a945ab4014a0af2595a3747890cc890ece7
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-24 14:23:46 -0400

    replaced FileField with imageField

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class UserProfile (models.Model):
-    image = models.FileField(upload_to="images")
+    image = models.ImageField(upload_to="images")
     
